74f0fb10835aae091b24f395a2d7029f1705b834
jonmobley <62940677+jonmobley@users.noreply.github.com>
2025-06-14 08:38:55 -0400

    Initial Commit

---
+++ Force/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Force
+//
+//  Created by Jon Mobley on 6/14/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Force/ForceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForceApp.swift
+//  Force
+//
+//  Created by Jon Mobley on 6/14/25.
+//
+
+import SwiftUI
+
+@main
+struct ForceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
